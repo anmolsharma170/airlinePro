1011d5fe84bfd0d766877825ec77c2437fc433db
AnmolSharma-git <148962440+AnmolSharma-git@users.noreply.github.com>
2026-04-16 02:23:15 +0530

    airplane middleware

--- src/routes/v1/airplane-routes.js
+++ src/routes/v1/airplane-routes.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const {AirplaneController} = require('../../controllers');
+const {AirplaneMiddlewares} = require('../../middlewares');
 const router = express.Router();
 
 //  /api/v1/airplanes and this is a POST request
-router.post('/', AirplaneController.createAirplane)
+router.post('/', 
+        AirplaneMiddlewares.validateCreateRequest,
+        AirplaneController.createAirplane)
 module.exports = router;
