c48fff197f86b2485fd65eba0370d7f5a7e2dd7c
AnmolSharma-git <148962440+AnmolSharma-git@users.noreply.github.com>
2026-04-21 18:04:18 +0530

    added middleware for creating city api

---
+++ src/middlewares/city-middleware.js
@@ -0,0 +1,21 @@
+// we dont want that if city name is empty it should do all the processing  
+// fistly to go to the controller than to service than to repository and all what is required
+// so in order to handle this task we can use a middleware
+const {StatusCodes} = require('http-status-codes');
+const {ErrorResponse} = require('../utils/common');
+const AppError = require('../utils/errors/app-error');
+function validateCreateRequest(req,res,next){
+    if(!req.body.name){
+        ErrorResponse.message = 'Something went wrong while creating city'; //putting these kind of raw strings is not a good practice but for our porsenal small project we are using it we can handle it in a json file later for more clarity
+        // one more reason is translation we sometimes directly send these messages from json using some translation mechanism and techniques so that client could understand in his/her languade about the error
+        // ErrorResponse.error = `Model Number not found in the incoming request in the correct form`
+        ErrorResponse.error = new AppError([`City name not found in the incoming request in the correct form`],StatusCodes.BAD_REQUEST);
+        return res
+                .status(StatusCodes.BAD_REQUEST)
+                .json(ErrorResponse);
+    }
+    next();
+}
+module.exports = {
+    validateCreateRequest
+} 
--- src/routes/v1/city-routes.js
+++ src/routes/v1/city-routes.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const {CityController} = require('../../controllers');
 const router = express.Router();
+const {CityMiddlewares} = require('../../middlewares');
 
 router.post('/',
+    CityMiddlewares.validateCreateRequest,
     CityController.createCity
 )
 module.exports= router;
